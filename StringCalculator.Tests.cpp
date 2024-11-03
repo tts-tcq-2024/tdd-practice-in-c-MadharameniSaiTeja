c0f9ecba59e49ffd21688a9f49e6b64122593aed
MadharameniSaiTeja <saiteja30930810@gmail.com>
2024-11-03 20:22:27 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -4,7 +4,7 @@
 TEST(StringCalculatorAddTests, ExpectZeroForEmptyInput) {
     int expectedresult = 0;
     const char* input = "Hello, world!";
-    int result = add(input);
+    int result = add(input); 
     ASSERT_EQ(result, expectedresult);
 }
 
